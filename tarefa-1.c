5b20474cbebc13186f63f04744ef45b9f3fa6821
Laíse Café <laisecaffe.sse@gmail.com>
2025-07-16 23:50:35 -0300

    Lógica do Buzzer

--- tarefa-1.c
+++ tarefa-1.c
@@ -23,6 +23,26 @@ volatile int estado = 0; // Estado do semáforo (0: vermelho, 1: amarelo, 2: ver
 struct repeating_timer timer; // Timer para controle do semáforo
 PIO pio = pio0; // PIO utilizado para controle da matriz
 uint sm = 0; // State machine para o PIO
+uint slice_buzzer; // Slice do buzzer
+
+// Configura o pino especificado para gerar sinal PWM com a frequência e duty cycle fornecidos.
+// A função calcula a divisão de clock necessária, configura o PWM e ativa o sinal no pino.
+void set_pwm_pin(uint pin, uint freq, uint duty_c) {
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(pin);
+    pwm_config config = pwm_get_default_config();
+    float div = (float)clock_get_hz(clk_sys) / (freq * 10000);
+    pwm_config_set_clkdiv(&config, div);
+    pwm_config_set_wrap(&config, 10000);
+    pwm_init(slice_num, &config, true);
+    pwm_set_gpio_level(pin, duty_c);
+}
+
+// Desativa o sinal PWM no pino especificado e o configura como saída digital comum com nível lógico baixo.
+void disable_pwm(uint pin) {
+    gpio_set_function(pin, GPIO_FUNC_SIO);
+    gpio_put(pin, 0);
+}
 
 // Função principal do semáforo
 bool semaforo(struct repeating_timer *t) {
@@ -39,7 +59,10 @@ bool semaforo(struct repeating_timer *t) {
             // Configura o contador regressivo na matriz de LEDs
             for(int i = 9; i > 0; i--){
                 print_num(i, pio, sm);
-                sleep_ms(1000);
+                set_pwm_pin(BUZZER, 500, 300);
+                sleep_ms(200);            
+                disable_pwm(BUZZER);
+                sleep_ms(800);    
             }
             clear_leds(pio, sm);
             
@@ -56,7 +79,11 @@ bool semaforo(struct repeating_timer *t) {
             // Configura o contador regressivo na matriz de LEDs
             for(int i = 6; i > 0; i--){
                 print_num(i, pio, sm);
-                sleep_ms(1000);
+                 int freq = 500 + (100 * (6 - i));
+                set_pwm_pin(BUZZER, freq, 300);
+                sleep_ms(200);           
+                disable_pwm(BUZZER);
+                sleep_ms(800); 
             }
             clear_leds(pio, sm);
             
@@ -75,6 +102,7 @@ bool semaforo(struct repeating_timer *t) {
                 print_num(i, pio, sm);
                 sleep_ms(1000);
             }
+            disable_pwm(BUZZER);
             clear_leds(pio, sm);
                        
             // Volta para o estado vermelho após 6 segundos
@@ -94,6 +122,8 @@ int main() {
     gpio_set_dir(LED_AZUL, GPIO_OUT);
     gpio_init(LED_VERMELHO);
     gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
 
     // Inicializa o programa PIO para controlar a matriz de LEDs
     uint deslocamento = pio_add_program(pio, &pio_matriz_program);
